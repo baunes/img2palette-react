884dae31e8273d92bbe7641986027ce1e15e78a0
Javier Bauset <javibauset@gmail.com>
2019-07-10 12:32:50 +0200

    Extract colors from image

--- src/components/Img2Palette.js
+++ src/components/Img2Palette.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import Vibrant from 'node-vibrant'
+
+import { Palette } from './Palette'
 
 export class Img2Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
 
   state = {
-    usedSearch: false
+    usedSearch: false,
+    colors: []
   }
 
   _renderDefault = () => {
     return (
-      <React.Fragment>
+      <div className="column is-6 is-offset-1">
         <h1 className="title is-2">
           Image To Palette
         </h1>
@@ -17,28 +25,57 @@ export class Img2Palette extends Component {
         </h2>
         <br />
         <p className="has-text-centered">
-          <button className="button is-medium is-info is-outlined">
+          <button className="button is-medium is-info is-outlined" onClick={this._extractPalette}>
             Extract
           </button>
         </p>
-      </React.Fragment>
+      </div>
     )
+  }
+
+  _renderColors = () => {
+    return (
+      <div className="column is-6 is-offset-1">
+        <Palette colors = {this.state.colors} />
+      </div>
+    )
+  }
+
+  _extractPalette = () => {
+    console.log("_extractPalette", this.myRef.current);
+    Vibrant
+      .from(this.myRef.current)
+      .getPalette()
+      .then((palette) => {
+        //Vibrant.rgbToHex()
+        console.log(palette);
+        this.setState((state) => ({
+          usedSearch: true,
+          colors: [
+            Vibrant.Util.rgbToHex(...palette.DarkMuted._rgb),
+            Vibrant.Util.rgbToHex(...palette.DarkVibrant._rgb),
+            Vibrant.Util.rgbToHex(...palette.LightMuted._rgb),
+            Vibrant.Util.rgbToHex(...palette.LightVibrant._rgb),
+            Vibrant.Util.rgbToHex(...palette.Muted._rgb),
+            Vibrant.Util.rgbToHex(...palette.Vibrant._rgb)
+          ]
+        }))
+      })
+      .catch((palette) => console.error("Error extracting colors", palette))
   }
 
   render() {
     return (
       <div className="container has-text-centered">
-        <div className="columns is-vcentered">
+        <div className="columns is-vcentered is-centered">
           <div className="column is-5">
             <figure className="image is-4by3">
-              <img className="App-img" src="https://picsum.photos/800/600/?random" alt="Description" />
+              <img ref={this.myRef} crossOrigin="anonymous" className="App-img" src="https://picsum.photos/800/600/?random" alt="Description" />
             </figure>
           </div>
-          <div className="column is-6 is-offset-1">
-            { this.state.usedSearch
-                ? this._renderDefault()
-                : this._renderDefault()}
-          </div>
+          { this.state.usedSearch
+                ? this._renderColors()
+                : this._renderDefault() }
         </div>
       </div>
     );
